4663452da24282efe5a6c858d39d1f8739d05e77
Carlota Ortega Vega <carlota.ortegavega@gmail.com>
2019-01-28 15:42:55 +0000

    Lab 2 Tasks 1-2

--- Lab02Mix/Lab02Code.py
+++ Lab02Mix/Lab02Code.py
@@ -12,7 +12,7 @@
 #           be imported.
 
 ###########################
-# Group Members: TODO
+# Group Members: Carlota Ortega Vega
 ###########################
 
 
@@ -133,10 +133,29 @@ def mix_client_one_hop(public_key, address, message):
     client_public_key  = private_key * G.generator()
 
     ## ADD CODE HERE
+    ## First get a shared key
+    shared_element = private_key * public_key
+    key_material = sha512(shared_element.export()).digest()
 
+    # Use different parts of the shared key for different operations
+    hmac_key = key_material[:16]
+    address_key = key_material[16:32]
+    message_key = key_material[32:48]
+
+    # Generate iv and encrypt the message and address
+    iv = b"\x00"*16
+    address_cipher = aes_ctr_enc_dec(address_key, iv, address_plaintext)
+    message_cipher = aes_ctr_enc_dec(message_key, iv, message_plaintext)
+
+    ## Check the HMAC
+    h = Hmac(b"sha512", hmac_key)        
+    h.update(address_cipher)
+    h.update(message_cipher)
+    expected_mac = h.digest()[:20]
+    
     return OneHopMixMessage(client_public_key, expected_mac, address_cipher, message_cipher)
 
-    
+
 
 #####################################################
 # TASK 3 -- Build a n-hop mix client.
